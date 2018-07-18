8494fea1082ad7146f5484474f1ececed90b0155
fodnb <Daniel.Mcghee6813@gmail.com>
2018-07-18 16:44:18 -0400

    Fixed date on DashboardPage to show date not unix

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
 
 const ExpenseListItem = ({ id, description, createdAt, amount}) => (
     <div>
         <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-        <p>{createdAt}</p>
+        <p>{moment(createdAt).format("MMMM Do YYYY")}</p>
         <p>{amount}</p>
         
     </div>
